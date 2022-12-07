fb117567fb0622385d554857e2876a26986d0392
pablokayro <pablokayro2413@gmail.com>
2022-12-06 23:58:23 -0300

    calculadora

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+// Usando o Switch Case
+
+class Aula13
+{
+    static void Main(string[]args){
+        //CALCULADORA SIMPLES 
+
+        double n1, n2, resultado;
+        int opcao;
+        Console.Write("CALCULADORA\nDigite um valor: ");
+        n1 = double.Parse(Console.ReadLine());
+        Console.Write("Digite outro valor: ");
+        n2 = double.Parse(Console.ReadLine());
+        Console.WriteLine("ESCOLHA UMA OPÇÃO");
+        Console.WriteLine("1 - SOMA");
+        Console.WriteLine("2 - SUBTRAÇÃO");
+        Console.WriteLine("3 - MULTIPLICAÇÃO");
+        Console.WriteLine("4 - DIVISÃO");
+        opcao = int.Parse(Console.ReadLine());
+        switch(opcao){
+            case 1:
+                resultado=n1+n2;
+                break;
+            case 2:
+                resultado=n1-n2;
+                break;
+            case 3:
+                resultado=n1*n2;
+                break;
+            case 4:
+                resultado=n1/n2;
+                break;
+            default:
+                resultado = -1;
+                break;
+        }
+        if (resultado<0){
+            Console.Write("Opção Inválida!");
+        }else{
+            Console.WriteLine($"O resultado da operação é: {resultado}");
+        }
+
+
+    
+
+    }
+}
